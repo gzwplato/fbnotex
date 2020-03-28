07cd6791b40f2feedf9675f69149560f55caea42
maxnd <maxnd@users.noreply.github.com>
2020-03-28 11:29:47 +0100

    Version 1.0.0

---
+++ unit4.pas
@@ -0,0 +1,222 @@
+// ***********************************************************************
+// ***********************************************************************
+// fbNotex 1.x
+// Author and copyright: Massimo Nardello, Modena (Italy) 2020.
+// Free software released under GPL licence version 3 or later.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. You can read the version 3
+// of the Licence in http://www.gnu.org/licenses/gpl-3.0.txt
+// or in the file Licence.txt included in the files of the
+// source code of this software.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// ***********************************************************************
+// ***********************************************************************
+
+unit Unit4;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  ExtCtrls, LazUTF8;
+
+type
+
+  { TfmOptions }
+
+  TfmOptions = class(TForm)
+    bnClose: TButton;
+    bnStFontColor: TButton;
+    edStSizePlus: TButton;
+    edStSizeLess: TButton;
+    edStSizeTitlesPlus: TButton;
+    edStSizeTitlesLess: TButton;
+    cbStFonts: TComboBox;
+    cdColorDialog: TColorDialog;
+    edBackup: TEdit;
+    edFBgbakPath: TEdit;
+    edFBLibPath: TEdit;
+    edServer: TEdit;
+    edPath: TEdit;
+    edPort: TEdit;
+    edStSize: TEdit;
+    edStSizeTitles: TEdit;
+    lbBackup: TLabel;
+    lbFBgbakPath: TLabel;
+    lbFBLibPath: TLabel;
+    lbServer: TLabel;
+    lbPath: TLabel;
+    lbPort: TLabel;
+    lbStSize: TLabel;
+    lbStSizeTitles: TLabel;
+    lnStFonts: TLabel;
+    procedure bnStFontColorClick(Sender: TObject);
+    procedure bnCloseClick(Sender: TObject);
+    procedure cbStFontsChange(Sender: TObject);
+    procedure edStSizeChange(Sender: TObject);
+    procedure edStSizeLessClick(Sender: TObject);
+    procedure edStSizePlusClick(Sender: TObject);
+    procedure edStSizeTitlesChange(Sender: TObject);
+    procedure edStSizeTitlesLessClick(Sender: TObject);
+    procedure edStSizeTitlesPlusClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  fmOptions: TfmOptions;
+
+implementation
+
+Uses Unit1;
+
+{$R *.lfm}
+
+{ TfmOptions }
+
+procedure TfmOptions.FormCreate(Sender: TObject);
+begin
+  cbStFonts.Items := Screen.Fonts;
+  cbStFonts.ItemIndex := 0;
+end;
+
+procedure TfmOptions.FormShow(Sender: TObject);
+begin
+  if fmMain.zcConnection.Connected = False then
+  begin
+    edFBLibPath.ReadOnly := False;
+    edFBgbakPath.ReadOnly := False;
+    edServer.ReadOnly := False;
+    edPort.ReadOnly := False;
+    edPath.ReadOnly := False;
+  end
+  else
+  begin
+    edFBLibPath.ReadOnly := True;
+    edFBgbakPath.ReadOnly := True;
+    edServer.ReadOnly := True;
+    edPort.ReadOnly := True;
+    edPath.ReadOnly := True;
+  end;
+  edStSize.Text := IntToStr(fmMain.dbText.Font.Size);
+  edStSizeTitles.Text := IntToStr(fmMain.sgTitles.Font.Size);
+  edFBLibPath.Text := fmMain.zcConnection.LibraryLocation;
+  edFBgbakPath.Text := stGBackDir;
+  edServer.Text := fmMain.zcConnection.HostName;
+  edPort.Text := IntToStr(fmMain.zcConnection.Port);
+  edPath.Text := fmMain.zcConnection.Database;
+  edBackup.Text := stBackupFile;
+end;
+
+procedure TfmOptions.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  with fmMain do
+  begin
+    if zcConnection.Connected = False then
+    begin
+      fmMain.zcConnection.LibraryLocation := edFBLibPath.Text;
+      stGBackDir := edFBgbakPath.Text;
+      zcConnection.HostName := edServer.Text;
+      zcConnection.Port := StrToInt(edPort.Text);
+      zcConnection.Database := edPath.Text;
+    end;
+    stBackupFile := edBackup.Text;
+    if UTF8LowerCase(edServer.Text) <> 'localhost' then
+    begin
+      fmMain.miToolsBackup.Enabled := False;
+      fmMain.miToolsRestore.Enabled := False;
+      fmMain.miToolsCompact.Enabled := False;
+      fmMain.bnBackup.Enabled := False;
+      fmMain.bnRestore.Enabled := False;
+    end
+    else
+    if fmMain.zcConnection.Connected = False then
+    begin
+      fmMain.miToolsBackup.Enabled := True;
+      fmMain.miToolsRestore.Enabled := True;
+      fmMain.miToolsCompact.Enabled := True;
+      fmMain.bnBackup.Enabled := True;
+      fmMain.bnRestore.Enabled := True;
+    end;
+  end;
+end;
+
+procedure TfmOptions.cbStFontsChange(Sender: TObject);
+begin
+  fmMain.dbText.Font.Name := cbStFonts.Text;
+  fmMain.sgTitles.Font.Name := cbStFonts.Text;
+end;
+
+procedure TfmOptions.edStSizeChange(Sender: TObject);
+begin
+  fmMain.dbText.Font.Size := StrToInt(edStSize.Text);
+end;
+
+procedure TfmOptions.edStSizeLessClick(Sender: TObject);
+begin
+  if StrToInt(edStSize.Text) > 7 then
+  begin
+    edStSize.Text := IntToStr(StrToInt(edStSize.Text) - 1);
+  end;
+end;
+
+procedure TfmOptions.edStSizePlusClick(Sender: TObject);
+begin
+  if StrToInt(edStSize.Text) < 256 then
+  begin
+    edStSize.Text := IntToStr(StrToInt(edStSize.Text) + 1);
+  end;
+end;
+
+procedure TfmOptions.edStSizeTitlesChange(Sender: TObject);
+begin
+  fmMain.sgTitles.Font.Size := StrToInt(edStSizeTitles.Text);
+end;
+
+procedure TfmOptions.edStSizeTitlesLessClick(Sender: TObject);
+begin
+  if StrToInt(edStSizeTitles.Text) > 7 then
+  begin
+    edStSizeTitles.Text := IntToStr(StrToInt(edStSizeTitles.Text) - 1);
+  end;
+end;
+
+procedure TfmOptions.edStSizeTitlesPlusClick(Sender: TObject);
+begin
+  if StrToInt(edStSizeTitles.Text) < 256 then
+  begin
+    edStSizeTitles.Text := IntToStr(StrToInt(edStSizeTitles.Text) + 1);
+  end;
+end;
+
+procedure TfmOptions.bnStFontColorClick(Sender: TObject);
+begin
+  cdColorDialog.Color := fmMain.dbText.Font.Color;
+  if cdColorDialog.Execute then
+  begin
+    fmMain.dbText.Font.Color := cdColorDialog.Color;
+    fmMain.sgTitles.Font.Color := cdColorDialog.Color;
+  end;
+end;
+
+procedure TfmOptions.bnCloseClick(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
+
---
+++ unit7.pas
@@ -0,0 +1,155 @@
+// ***********************************************************************
+// ***********************************************************************
+// fbNotex 1.x
+// Author and copyright: Massimo Nardello, Modena (Italy) 2020.
+// Free software released under GPL licence version 3 or later.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. You can read the version 3
+// of the Licence in http://www.gnu.org/licenses/gpl-3.0.txt
+// or in the file Licence.txt included in the files of the
+// source code of this software.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// ***********************************************************************
+// ***********************************************************************
+
+unit Unit7;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus,
+  Clipbrd, ExtCtrls, ZDataset;
+
+type
+
+  { TfmInsertID }
+
+  TfmInsertID = class(TForm)
+    bnOK: TButton;
+    bnCancel: TButton;
+    bnPaste: TButton;
+    edID: TEdit;
+    lbTitle: TLabel;
+    lbIDKind: TLabel;
+    pmMenu: TPopupMenu;
+    Shape1: TShape;
+    procedure bnPasteClick(Sender: TObject);
+    procedure edIDKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+    iNoteSect: SmallInt;
+
+  end;
+
+var
+  fmInsertID: TfmInsertID;
+
+implementation
+
+uses Unit1;
+
+{$R *.lfm}
+
+{ TfmInsertID }
+
+procedure TfmInsertID.edIDKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if ((key = Ord('V')) and (Shift = [ssMeta])) then
+  begin
+    key := 0;
+    if bnPaste.Enabled = True then
+    begin
+      bnPasteClick(nil);
+    end;
+  end
+  else
+  if key = 13 then
+  begin
+    ModalResult := mrOK;
+  end
+  else
+  if (((key < 48) or (key > 57)) and (key <> 8)) then
+  begin
+    key := 0;
+  end
+  else
+  begin
+    key := 0;
+    lbTitle.Caption := '';
+  end;
+end;
+
+procedure TfmInsertID.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  if key = 27 then
+  begin
+    key := 0;
+    ModalResult := mrCancel;
+  end;
+end;
+
+procedure TfmInsertID.bnPasteClick(Sender: TObject);
+  var myDataset: TZQuery;
+begin
+  edID.Clear;
+  lbTitle.Caption := '';
+  edID.PasteFromClipboard;
+  try
+    myDataset := TZQuery.Create(Self);
+    myDataset.Connection := fmMain.zqNotebooks.Connection;
+    if iNoteSect = 0 then
+    begin
+      myDataset.Sql.Add('Select Notebooks.Title from Notebooks');
+      myDataset.Sql.Add('where Notebooks.ID = ' + edID.Text);
+    end
+    else
+    if iNoteSect = 1 then
+    begin
+      myDataset.Sql.Add('Select Sections.Title from Sections');
+      myDataset.Sql.Add('where Sections.ID = ' + edID.Text);
+    end
+    else
+    if iNoteSect = 2 then
+    begin
+      myDataset.Sql.Add('Select Notes.Title from Notes');
+      myDataset.Sql.Add('where Notes.ID = ' + edID.Text);
+    end;
+    myDataSet.Open;
+    lbTitle.Caption := myDataset.FieldByName('Title').Value;
+    myDataset.Close;
+  finally
+    myDataset.Free;
+  end;
+end;
+
+procedure TfmInsertID.FormShow(Sender: TObject);
+  var i: integer;
+begin
+  edID.SetFocus;
+  edID.SelectAll;
+  if TryStrToInt(Clipboard.AsText, i) = True then
+  begin
+    bnPaste.Enabled := True;
+  end
+  else
+  begin
+    bnPaste.Enabled := False;
+  end;
+end;
+
+end.
+
